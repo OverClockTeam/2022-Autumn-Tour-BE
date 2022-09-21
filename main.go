4b8af26ad261d4c155a60dd28dedc7b77aa5a17b
kai <1424431045@qq.com>
2022-09-22 00:34:09 +0800

    爬虫完善

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
 func main() {
-	//fmt.Printf("Hello Go")
-	_ = downloadMusic("爸爸妈妈")
+	for i := 0; i < 999; i++ {
+		res := RandStringRunes(2)
+		fmt.Println(res)
+		_ = downloadMusic(res)
+	}
+
 }
