e81c63a2a6f00f287d6d9c1f9d7854e5f0d6753e
agieeo <113911708+agieeo@users.noreply.github.com>
2022-09-21 23:07:19 +0800

    start
    just a start

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/smtp"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+type User struct {
+	username   string
+  email      string
+  passward   string
+}
