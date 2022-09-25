b65ab1ad876d6b855cdd46f2c77f179f7f2f22dc
Lizunsen <1444523394@qq.com>
2022-09-26 00:49:55 +0800

    补充一些函数实现

---  api/v1/user.go
+++  api/v1/user.go
@@ -1,17 +1,36 @@
 package v1
 
 import (
+	"OverClock/model"
+	"OverClock/utils/errmsg"
 	"github.com/gin-gonic/gin"
 	_ "github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
+var code int
 //查询用户是否存在
 func UserExit(c *gin.Context){
 
 }
 //添加用户
 func AddUser(c *gin.Context){
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckUser(data.Username)
+	if code == errmsg.SUCCEED{
+		model.CreateUser(&data)
+	}
+	if code == errmsg.ERROR_USERNAME_USED{
+		code = errmsg.ERROR_USERNAME_USED
+	}
 
+	c.JSON(http.StatusOK,gin.H{
+		"status":code,
+		"data":data,
+		"message":errmsg.GetErrMsg(code),
+	})
 }
 //查询单个用户
 func GetUser(c *gin.Context){
@@ -19,7 +38,22 @@ func GetUser(c *gin.Context){
 }
 //查询用户列表
 func GetUsers(c *gin.Context){
+	pageSize,_ :=strconv.Atoi(c.Query("pagesize"))
+	pageNum,_ :=strconv.Atoi(c.Query("pageNum"))
 
+	if pageSize == 0{
+		pageSize = -1
+	}
+	if pageNum == 0 {
+		pageNum = -1
+	}
+	data := model.GetUsers(pageSize,pageNum)
+	code = errmsg.SUCCEED
+	 c.JSON(http.StatusOK,gin.H{
+	 	"status":code,
+	 	"data":data,
+	 	"message":errmsg.GetErrMsg(code),
+	 })
 }
 //编辑用户
 func EditUser(c *gin.Context){
--- model/User.go
+++ model/User.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"OverClock/utils/errmsg"
+	"github.com/jinzhu/gorm"
+)
 
 type User struct{
 	gorm.Model
@@ -9,5 +12,35 @@ type User struct{
 	Email string  `gorm:"type:varchar(30);not null " json:"email"`
 	Role int  `gorm:"type:int;not null " json:"role"`
 }
+//查询用户是否存在
+func CheckUser(name string)(code int){
+	var users User
+	db.Select("id").Where("username = ?",name).First(&users) // 在数据表中查询
+	if users.ID > 0{
+		return errmsg.ERROR_USERNAME_USED
+	}
+	return errmsg.SUCCEED
+}
+//添加用户
+func CreateUser(data *User)(code int){
+	error := db.Create(&data).Error
+	if error != nil{
+		return errmsg.ERRO
+	}
+	return errmsg.SUCCEED
+}
+
+//查询用户列表
+func GetUsers(pageSize int,pageNum int) []User {
+	var users []User
+	err = db.Limit(pageSize).Offset((pageNum-1)*pageSize).Find(&users).Error
+	if err != gorm.ErrRecordNotFound{
+		return nil
+	}
+	return users
+}
+
+//编辑用户
 
 
+//删除用户
