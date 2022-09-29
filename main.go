b1e6eaed89aec02bebf9da5527fea63efd0be285
Emilia02 <100981323+Emilia02Mai@users.noreply.github.com>
2022-09-29 08:40:57 -0700

    Update main.go

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
-//http://localhost:8080/upload -F "file=@/Users/appleboy/test.zip" \
-//-H "Content-Type: multipart/form-data"
+//http://localhost:8080/upload -F "file=@/usr/local/games/test.zip" -H "Content-Type: multipart/form-data"
 
 func main() {
 	router := gin.Default()
@@ -22,18 +23,38 @@ func main() {
 		// Source
 		file, err := c.FormFile("file")
 		if err != nil {
-			c.String(http.StatusBadRequest, "get form err: %s", err.Error())//400
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error()) //400
 			return
 		}
 
 		filename := filepath.Base(file.Filename)
 
 		if err := c.SaveUploadedFile(file, filename); err != nil {
-			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())//400
+			c.String(http.StatusBadRequest, "upload file err: %s", err.Error()) //400
 			return
 		}
 
-		c.String(http.StatusOK, "File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email)//200
+		c.String(http.StatusOK, "File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email) //200
 	})
+	getcookie()
 	router.Run(":8080")
+}
+
+func getcookie() {
+
+	router := gin.Default()
+
+	router.GET("/cookie", func(c *gin.Context) {
+
+		cookie, err := c.Cookie("gin_cookie")
+
+		if err != nil {
+			cookie = "NotSet"
+			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+		}
+
+		fmt.Printf("Cookie value: %s \n", cookie)
+	})
+
+	router.Run()
 }
