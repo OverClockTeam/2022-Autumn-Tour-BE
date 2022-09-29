c446b297aa8091525ea128f346525bb6ecaf39f7
Emilia02 <100981323+Emilia02Mai@users.noreply.github.com>
2022-09-29 07:04:21 -0700

    Update main.go
    file up

--- main.go
+++ main.go
@@ -1,77 +1,39 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"path/filepath"
+
+	"github.com/gin-gonic/gin"
 )
 
-type Person struct {
-	ID   string `uri:"id" binding:"required,uuid"`
-	Name string `uri:"name" binding:"required"`
-}
+//http://localhost:8080/upload -F "file=@/Users/appleboy/test.zip" \
+//-H "Content-Type: multipart/form-data"
 
 func main() {
-	route := gin.Default()
-	route.GET("/:name/:id", func(c *gin.Context) {
-		var person Person
-		if err := c.ShouldBindUri(&person); err != nil {
-			c.JSON(400, gin.H{"msg": err.Error()})
-			return
-		}
-		c.JSON(200, gin.H{"name": person.Name, "uuid": person.ID})
-	})
-	route.Run(":8088")
-
 	router := gin.Default()
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
-	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.MaxMultipartMemory = 4 << 20 // 4 MiB
 	router.Static("/", "./public")
+
 	router.POST("/upload", func(c *gin.Context) {
 		name := c.PostForm("name")
 		email := c.PostForm("email")
-
 		// Source
 		file, err := c.FormFile("file")
 		if err != nil {
-			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())//400
 			return
 		}
 
 		filename := filepath.Base(file.Filename)
+
 		if err := c.SaveUploadedFile(file, filename); err != nil {
-			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())//400
 			return
 		}
 
-		c.String(http.StatusOK, "File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email)
+		c.String(http.StatusOK, "File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email)//200
 	})
 	router.Run(":8080")
 }
-
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    fmt.Println(string(body))
-    fmt.Println(resp.StatusCode)
-    if resp.StatusCode == 200 {
-        fmt.Println("ok")
-    }
-
-func uploadHandler(c *gin.Context) {
-	header, err := c.FormFile(uploadFileKey)
-	if err != nil {
-		//ignore
-	}
-	dst := header.Filename
-  // gin 简单做了封装,拷贝了文件流
-	if err := c.SaveUploadedFile(header, dst); err != nil {
-		// ignore
-	}
-}
-
-//基本的GET请求
- 
-
-
-
