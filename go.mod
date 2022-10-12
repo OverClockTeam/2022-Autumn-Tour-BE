e7ed68f471b1e5bd935d00f95569a8dd0e1ee2c5
王健 <wangjian@wangjiandeMacBook-Air.local>
2022-10-12 23:26:09 +0800

    更新了检查作业系统，新增了班长账号，班长可以查看同班同学谁没交作业并发送邮件提醒

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module work
 
 go 1.19
 
+require github.com/go-gomail/gomail v0.0.0-20160411212932-81ebce5c23df
+
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
@@ -23,5 +25,7 @@ require (
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/gomail.v2"
 )
 
 //定义全局数据
@@ -16,10 +18,43 @@ var u User
 
 //定义结构体
 type User struct {
-	Status bool
+	Status int	//0是未登录，1是普通用户，2是班长
 	Username string
 	Password string
+	Class int
 	Email string
+}
+
+type Homework struct {
+	Username string
+	Subject string
+	Email string
+}
+
+
+//定义邮箱
+func SendMail(mailTo []string, subject string, body string) error {
+ 
+    mailConn := map[string]string{
+        "user": "wj13985781016@163.com",
+        "pass": "NPGZEFRFHVRMRUHO",
+        "host": "smtp.163.com",
+        "port": "465",
+    }
+ 
+    port, _ := strconv.Atoi(mailConn["port"]) //转换端口类型为int
+ 
+    m := gomail.NewMessage()
+ 
+    m.SetHeader("From",  m.FormatAddress(mailConn["user"], "班长")) //这种方式可以添加别名，即“XX官方”
+    m.SetHeader("To", mailTo...)    //发送给多个用户
+    m.SetHeader("Subject", subject) //设置邮件主题
+    m.SetBody("text/html", body)    //设置邮件正文
+ 
+    d := gomail.NewDialer(mailConn["host"], port, mailConn["user"], mailConn["pass"])
+ 
+    err := d.DialAndSend(m)
+    return err
 }
 
 //连接数据库
@@ -36,6 +71,7 @@ func InitDB() (err error) {
 	return nil
 }
 
+
 //接收文件
 func Upload(c *gin.Context) {
 	//接受文件
@@ -49,14 +85,23 @@ func Upload(c *gin.Context) {
 		return
 	}
 	log.Println(file.Filename)
-	//上传文件到指定目录
-	dst := fmt.Sprintf("./subject_file/%s/%s", subject, file.Filename)
+
+	//第一次上传登记上传信息
+	s := "select * from homework where username = ? and subject = ?"
+	err = db.QueryRow(s, u.Username, subject).Scan()
+	if err != nil {
+		s := "insert into homework (username, subject, class) values(?,?,?)"
+		r, _ := db.Exec(s, u.Username, subject, u.Class)
+		fmt.Printf("r: %v\n", r);
+	} 
+
+	//上传文件到指定目录,格式为 班级_用户名_文件名
+	dst := fmt.Sprintf("./subject_file/%s/%s", subject, strconv.Itoa(u.Class) + "_" + u.Username + "_" + file.Filename)
 	c.SaveUploadedFile(file, dst)
 	c.HTML(http.StatusOK, "html/jump.tmpl", gin.H{
-		"Url" : "/",
+		"Url" : "/login/index",
 		"Context" : "上传文件成功！请稍后",
 	})
-	
 }
 
 //注册页面
@@ -68,9 +113,11 @@ func Register(c *gin.Context) {
 	f.POST("/register/get", func(c *gin.Context) {
 		//获取上传的信息
 		var u User
+		var err error
 		u.Username = c.PostForm("username")
 		u.Password = c.PostForm("password1")
 		u.Email = c.PostForm("email")
+		u.Class, _ = strconv.Atoi(c.PostForm("class"))
 		password1 := c.PostForm("password2")
 
 		//重复密码不相等时重新进入网页
@@ -84,7 +131,7 @@ func Register(c *gin.Context) {
 
 		//用户名存在时重新进入页面
 		s := "select * from users where username = ?"
-		err := db.QueryRow(s, u.Username).Scan(&u.Username, &u.Username, &u.Username)
+		err = db.QueryRow(s, u.Username).Scan(&u.Username, &u.Username, &u.Username)
 		if err == nil {
 			c.HTML(http.StatusOK, "html/jump.tmpl", gin.H{
 				"Url" : "/",
@@ -94,8 +141,9 @@ func Register(c *gin.Context) {
 		}
 
 		//将数据上传到数据库
-		s = "insert into users (username, password, email) values(?,?,?)"
-		r, err := db.Exec(s, u.Username, u.Password, u.Email)
+		u.Status, _ = strconv.Atoi(c.PostForm("monitor"))
+		s = "insert into users (username, password, email, class, monitor) values(?,?,?,?,?)"
+		r, err := db.Exec(s, u.Username, u.Password, u.Email, u.Class, u.Status)
 		if err != nil {
 			fmt.Printf("err: %v\n", err)
 			return
@@ -112,6 +160,7 @@ func Register(c *gin.Context) {
 	})
 }
 
+//登陆页面
 func Login(c *gin.Context) {
 
 	//接收数据
@@ -120,35 +169,114 @@ func Login(c *gin.Context) {
 	
 	//判断是否存在账号
 	s := "select * from users where username = ? and password = ?"
-	err := db.QueryRow(s, u.Username, u.Password).Scan(&u.Username, &u.Password, &u.Email)
+	err := db.QueryRow(s, u.Username, u.Password).Scan(&u.Username, &u.Password, &u.Email, &u.Class, &u.Status)
 	if err != nil {
-		u.Status = false
+		u.Status = 0
 		c.HTML(http.StatusOK, "html/jump.tmpl", gin.H{
 			"Url" : "/",
 			"Context" : "用户名或密码不正确",
 		})
 	} else {
-		u.Status = true
 		c.HTML(http.StatusOK, "html/jump.tmpl", gin.H{
-			"Url" : "/index",
+			"Url" : "/login/index",
 			"Context" : "登录成功！请稍后",
 		})
 	}
+
+	//登录成功后的主界面
+	f.Any("/login/index", Index)
 }
 
+//检查作业
+func Check(c *gin.Context) {
+	//接受数据
+	subject := c.PostForm("subject")
+	len := 0
+	
+	//找出自己班级没交某个作业的人并输出
+	s := "select users.username, users.email, homework.username from users left join homework on users.username = homework.username and users.class = ? and homework.subject = ?"
+	r, _ := db.Query(s, u.Class, subject)
+	var users [] Homework
+	var u1 Homework
+	for r.Next() {
+		r.Scan(&u1.Username, &u1.Email, &u1.Subject)
+		//若没有提交则左右表名字不匹配
+		if u1.Subject != u1.Username  {
+			u1.Subject = subject
+			users = append(users, u1)
+			len++
+		}
+	}
+	
+	//输出到html页面
+	c.HTML(http.StatusOK, "html/index.tmpl", gin.H{
+		"res": users,
+		"name" : u.Username + "班长",
+		"monitor" : 1,
+	})
+	
+	//发送邮件 
+	f.POST("/login/index/check/sendmail", func(c *gin.Context) {
+		//定义收件人
+		mailTo := []string{
+			"2307742375@qq.com",
+		}
+		/* for i := 0; i < len; i++ {
+			mailTo = append(mailTo, users[i].Email)
+		} */
+		//邮件主题为"Hello"
+		subject := "交作业"
+		// 邮件正文
+		body := "请及时交" + subject + "作业！" + "来自" + u.Username + "班长"
+	 
+		err := SendMail(mailTo, subject, body)
+		if err != nil {
+			log.Println(err)
+			fmt.Println("send fail")
+			return
+		}
+	 
+		fmt.Println("send successfully")
+	})
+
+}
+
+//主界面渲染
 func Index(c *gin.Context) {
-	if u.Status {
-		c.HTML(http.StatusOK, "html/index.tmpl", u.Username)
-	} else {
+	//阻止没有登录的访问,并分别渲染普通用户和班长的页面
+	switch u.Status {
+	case 0:
 		c.HTML(http.StatusOK, "html/jump.tmpl", gin.H{
 			"Url" : "/",
 			"Context" : "请先登录！",
 		})
+	case 1: //普通用户
+		c.HTML(http.StatusOK, "html/index.tmpl", gin.H{
+			"name" : u.Username,
+			"monitor" : 0,
+		})
+	case 2:	//班长
+		//导出没有交作业的同班同学并制作成表格
+		c.HTML(http.StatusOK, "html/index.tmpl", gin.H{
+			"name" : u.Username + "班长",
+			"monitor" : 1,
+			"res" : nil,
+		})
 	}
+
+	//接受作业
+	f.POST("/login/index/upload", Upload)
+
+	//检查作业
+	f.POST("/login/index/check", Check)
+
+	//退出登录
+	f.POST("/login/index/logout", Logout)
 }
 
+//登出
 func Logout(c *gin.Context) {
-	u.Status = false
+	u.Status = 0
 	c.HTML(http.StatusOK, "html/jump.tmpl", gin.H{
 		"Url" : "/",
 		"Context" : "正在登出",
@@ -160,7 +288,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	u.Status = false //初始化用户状态
+	u.Status = 0 //初始化用户状态
 
 	//获取路由对象
 	f = gin.Default()
@@ -178,15 +306,6 @@ func main() {
 
 	//处理点击注册后的情况
 	f.POST("/register", Register)
-
-	//接受文件
-	f.POST("/upload", Upload)
-	
-	//登录成功后的主界面
-	f.Any("/index", Index)
-
-	//退出登录
-	f.POST("/logout", Logout)
 
 	f.Run()
 }
